717b6c2ed6c15d6ee3837fa56b69cf91e1e7ca7d
RoyalAdvisor <johnsonepc@gmail.com>
2022-03-10 11:46:33 +0200

    updated user verifification before editing post

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 const post = require("../models/postModel");
+const user = require("../models/userModel");
+const comment = require("../models/commentModel");
 const { retrievePost } = require("../middleware/retriever");
 const { retrieveUser } = require("../middleware/retriever");
 const verifyAcc = require("../middleware/authJWT");
+const dotenv = require("dotenv");
+dotenv.config();
+
+//Blog posts
 
 router.get("/", verifyAcc, async (req, res) => {
   try {
@@ -39,10 +45,11 @@ router.put(
   "/:id",
   [verifyAcc, retrievePost, retrieveUser],
   async (req, res) => {
-    if (res.user.username != req.body.created_by)
+    if (res.user.username !== res.post.created_by) {
       return res
-        .status(400)
-        .send({ message: "You do not have permission to edit this post." });
+        .status(401)
+        .send({ message: "You are not authorized to delete this post." });
+    }
     if (req.body.main_image != null) res.post.main_image = req.body.main_image;
     if (req.body.title != null) res.post.title = req.body.title;
     if (req.body.subtitle != null) res.post.subtitle = req.body.subtitle;
@@ -62,13 +69,62 @@ router.delete(
   "/:id",
   [verifyAcc, retrievePost, retrieveUser],
   async (req, res) => {
-    if (res.user.username != req.body.created_by)
+    if (res.user.username !== res.post.created_by) {
       return res
-        .status(400)
-        .send({ message: "You do not have permission to delete this post." });
+        .status(401)
+        .send({ message: "You are not authorized to delete this post." });
+    }
     try {
       await res.post.remove();
       res.status(200).send({ message: "Post deleted successfully." });
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  }
+);
+
+//Comment Section
+
+router.get("/:id/comments", [verifyAcc, retrievePost], (req, res) => {
+  return res.send(res.post.comments);
+});
+
+router.post(
+  "/:id/comments/create",
+  [verifyAcc, retrievePost, retrieveUser],
+  async (req, res) => {
+    let userName = res.user.username;
+    let newComment = new comment({
+      content: req.body.content,
+      posted_by: userName,
+    });
+    let comments = res.post.comments;
+    let addedToComments = false;
+    if (!addedToComments) comments.push(newComment);
+    try {
+      const updatedPost = res.post.save(comments);
+      res.status(200).send({ message: "Comment created" });
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  }
+);
+
+router.delete(
+  "/:id/comments/delete",
+  [verifyAcc, retrievePost],
+  async (req, res) => {
+    let commentList = res.post.comments;
+    let index = commentList
+      .forEach((comment) => {
+        console.log(comment._id);
+      })
+      .indexOf(comment._id);
+    try {
+      commentList.splice(index, 1);
+      console.log(commentList);
+      const updatedPost = res.post.save(commentList);
+      res.status(200).send({ message: "Comment deleted successfully." });
     } catch (error) {
       res.status(500).send({ message: error.message });
     }
--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const app = express();
 const mongoose = require("mongoose");
 const userRoute = require("./routes/userRoute");
 const postRoute = require("./routes/postRoute");
-const authRoute = require("./routes/authRoute");
-const commentRoute = require("./routes/commentRoute");
 
 app.use(express.json());
 
@@ -14,14 +12,13 @@ mongoose.connect(process.env.connection, { useNewUrlParser: true }, () => {
   console.log("Connected to MongoDB database.");
 });
 
-app.use("/auth", authRoute);
 app.use("/users", userRoute);
 app.use("/posts", postRoute);
-app.use("/posts", commentRoute);
 
 app.get("/", (req, res) => {
   res.send("Rest API for The Random Blog.");
 });
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is active on port ${port}`);
